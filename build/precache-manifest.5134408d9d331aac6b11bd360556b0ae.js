293e1feeba7d369220d4f4c321e4815bd11eb51b
Akhil Kumar Singh <52193850+akhil27styles@users.noreply.github.com>
2020-11-12 14:37:18 +0530

    Upload build

---
+++ build/precache-manifest.5134408d9d331aac6b11bd360556b0ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4b0ad2cf1de0877c5f5ec1d4c47327",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba8ec6efd19b593b154e",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "a9860be8dce75a9b1271",
+    "url": "/static/css/main.a3886233.chunk.css"
+  },
+  {
+    "revision": "ba8ec6efd19b593b154e",
+    "url": "/static/js/2.79a32aec.chunk.js"
+  },
+  {
+    "revision": "e3568650101a694ad5643b0230958c9a",
+    "url": "/static/js/2.79a32aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9860be8dce75a9b1271",
+    "url": "/static/js/main.eaafb77c.chunk.js"
+  },
+  {
+    "revision": "1dc6fbfb725e7cb780f6",
+    "url": "/static/js/runtime-main.c2fb6b75.js"
+  },
+  {
+    "revision": "7284e3ccdf3bf0f9381e37b53f66d772",
+    "url": "/static/media/library-438389.7284e3cc.jpg"
+  }
+]);
